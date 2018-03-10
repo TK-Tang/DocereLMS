64988f66f5320f185690970081c2a63b5ab13e9c
TKTang <tk.gq.tang@gmail.com>
2018-03-11 01:25:03 +1100

    Tweeks here and there for auth

--- DocereLMS - Javascript/DocereLMSAPI/middleware/authentication-service.js
+++ DocereLMS - Javascript/DocereLMSAPI/middleware/authentication-service.js
@@ -2,13 +2,17 @@ const Models = require("../models");
 const Passport = require("passport");
 const Responses = require("../helpers/response");
 
-exports.isAuthenticated = function (req, res, next){
+exports.isLoggedIn= function (req, res, next){
     if (req.isAuthenticated()){ return next(); }
+
+    // Someone is not logged in
     res.status(200).send(Responses.success("Redirect request as it has no token."));
 }
 
-exports.isNotAuthenticated = function (req, res, next){
+exports.isNotLoggedIn = function (req, res, next){
     if (!req.isAuthenticated()){ return next(); }
+
+    // Someone logged in tried to go to sign up page
     res.status(200).send(Responses.success("Redirect request as it has a token."));
 }
 
@@ -29,17 +33,38 @@ exports.isStudent = function(req, res, next){
 
     if (!req.isAuthenticated()) { res.status(400).send(Responses.fail("Authentication failure: Not authenticated")); }
 
-    Models.Users.get(req.body.email, Models).then(function(user){
+    Models.Users.get(req.user.email, Models).then(function(user){
         if (!user){ 
             console.log("Email not found in database for user logging in");
             return res.status(400).send(Responses.fail("Authentication failure"));
         }
 
-        if(!user.Roles){
+        if(!user.Role){
             console.log("User role not found in database for using logging in");
             return res.status(400).send(Responses.fail("Authentication failure"));
-        } else if (user.Roles.rank != "student"){
-            console.log("User is not a student. They have a rank of: " + user.Roles.rank);
+        } else if (user.Role.rank != "student"){
+            console.log("User is not a student. They have a rank of: " + user.Role.rank);
+            return res.status(400).send(Responses.fail("Authentication failure"));
+        }
+
+        return next();
+    });
+}
+
+exports.isStudent = function(req, res, next){
+    if (!req.isAuthenticated()) { res.status(400).send(Responses.fail("Authentication failure: Not authenticated")); }
+
+    Models.Users.get(req.user.email, Models).then(function(user){
+        if (!user){ 
+            console.log("Email not found in database for user logging in");
+            return res.status(400).send(Responses.fail("Authentication failure"));
+        }
+
+        if(!user.Role){
+            console.log("User role not found in database for using logging in");
+            return res.status(400).send(Responses.fail("Authentication failure"));
+        } else if (user.Role.rank != "admin"){
+            console.log("User is not an admin. They have a rank of: " + user.Role.rank);
             return res.status(400).send(Responses.fail("Authentication failure"));
         }
 
--- DocereLMS - Javascript/DocereLMSAPI/routes/authentication-router.js
+++ DocereLMS - Javascript/DocereLMSAPI/routes/authentication-router.js
@@ -3,15 +3,15 @@ const AuthenticationService = require("../middleware/authentication-service");
 const Responses = require("../helpers/response");
 
 module.exports = function(app, passport){
-    app.get("/auth/signup", AuthenticationService.isNotAuthenticated, AuthenticationController.signUp);
-    app.post("/auth/signup", AuthenticationService.isNotAuthenticated, passport.authenticate("local-signup"), (req, res) => {
+    app.get("/auth/signup", AuthenticationService.isNotLoggedIn, AuthenticationController.signUp);
+    app.post("/auth/signup", AuthenticationService.isNotLoggedIn, passport.authenticate("local-signup"), (req, res) => {
         res.status(200).send(Responses.success("success"));
     });
 
-    app.get("/auth/signin", AuthenticationService.isNotAuthenticated, AuthenticationController.signIn);
-    app.post("/auth/signin", AuthenticationService.isNotAuthenticated, passport.authenticate("local-signin"), (req, res) => {
+    app.get("/auth/signin", AuthenticationService.isNotLoggedIn, AuthenticationController.signIn);
+    app.post("/auth/signin", AuthenticationService.isNotLoggedIn, passport.authenticate("local-signin"), (req, res) => {
         res.status(200).send(Responses.success("success"));
     })
 
-    app.get("/auth/signout", AuthenticationService.isAuthenticated, AuthenticationController.signOut);
+    app.get("/auth/signout", AuthenticationService.isLoggedIn, AuthenticationController.signOut);
 }
